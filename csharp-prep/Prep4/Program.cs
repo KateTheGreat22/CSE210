e08cfb7e6cec29dc1989f8426a058cf2d01337b6
Kaitlyn Johnson <you@example.com>
2025-09-24 20:59:58 -0600

    Prep 4 Completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("This program will add your numbers to the list until 0 is inputed.");
+
+        List<int> numbers = new List<int>();
+        int total = 0;
+
+        string response = "yes";
+        while (response == "yes")
+        {
+            Console.Write("What number would you like to add? ");
+            string guess = Console.ReadLine();
+            int number = int.Parse(guess);
+
+            if (number == 0)
+            {
+                response = "no";
+            }
+            else
+            {
+                numbers.Add(number);
+                total += number;
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+            double average = (double)total / numbers.Count;
+            Console.WriteLine($"Your total is {total}");
+            Console.WriteLine($"Your average is {average}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered.");
+        }
+        int highest = numbers[0];
+
+        foreach (int numb in numbers)
+        {
+            if (numb > highest)
+            {
+                highest = numb;
+            }
+        }
+        Console.WriteLine($"Your highest number is {highest}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+        DisplayName();
+        DisplayNumber();
+        DisplayYear();
+
+        void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        Console.Write("What is your name?");
+        string name = Console.ReadLine();
+
+        void DisplayName(string name)
+        {
+            Console.WriteLine($"Welcome {name}!");
+        }
+
+        Console.Write("What is your favorite number?");
+        string number = Console.ReadLine();
+        int numbers = int.Parse(number);
+
+        void DisplayNumber(int numbers)
+        {
+            Console.WriteLine($"Welcome {name}!");
+        }
+
+        Console.Write("What year were you born?");
+        int year = Console.ReadLine();
+        int years = int.Parse(year);
+
+        void DisplayYear(int years)
+        {
+            Console.WriteLine($"Welcome {name}!");
+        }
+
     }
 }
