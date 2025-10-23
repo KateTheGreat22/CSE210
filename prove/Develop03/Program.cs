99be9e2ca076a0f49390b288c684cb688ec83300
Kaitlyn Johnson <Kaitlynjohnson12892@gmail.com>
2025-10-22 19:07:02 -0600

    Edited and Final

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
+//To exceed requirements, the code will only select words at random that have not already been hidden. 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Acts", 4, 20);
+
+        Scripture scripture = new Scripture(reference, "For we cannot but speak the things which we have seen and heard.");
+        
+        string response = "";
+
+        while (response != "quit" && !scripture.AllWordsHidden())
+        {
+            Console.Clear();
+            scripture.DisplayWithReference();
+
+
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit:");
+            response = Console.ReadLine();
+
+            if (response != "quit")
+            {
+                scripture.HideRandomWord();
+            }
+        }
+
+        // Display one final time to show all words hidden
+        if (response != "quit")
+        {
+            Console.Clear();
+            scripture.DisplayWithReference();
+        }
+
+        Console.WriteLine("\nProgram is finished. Have a nice day!");
     }
 }
