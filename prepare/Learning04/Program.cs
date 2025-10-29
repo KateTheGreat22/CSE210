cb5657e6032d83b7fca571c1cb2407634e7eefe6
Kaitlyn Johnson <Kaitlynjohnson12892@gmail.com>
2025-10-28 20:08:11 -0600

    Prepwork Done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Kaitlyn Johnson", "Addition");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Bryce Miguel", "Fractions", "4.5", "1-3");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WriteAssignment a3 = new WriteAssignment("Lord Hamlet", "World History", "WWII");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
